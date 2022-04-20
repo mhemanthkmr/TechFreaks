c606cadd92ad2e97256d61252032d5e4f3f03673
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-04-20 23:38:50 +0530

    Created file access Alerts

---
+++ message.php
@@ -0,0 +1,5 @@
+<div class="alert alert-success alert-dismissible fade show border-0" role="alert">
+					<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button> User profile is not complete
+			  </div>
