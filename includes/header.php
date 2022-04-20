fc3d8119307210eb1c17d5d804f88daea97bff51
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-04-20 23:40:46 +0530

    Updated Author

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="">
-	<meta name="author" content="Jozef Dvorský - creatingo.com">
+	<meta name="author" content="HemanthKumar M">
 
 	<title><?php if(isset($title)) echo $title.' - TechFreaks IoT'?></title>
 
