a0727a85dae4f707994d58cb513b5c15f1c2b67b
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-04-23 22:38:26 +0530

    Added class Implementing FInally

--- code/user.Class.php
+++ code/user.Class.php
@@ -1,9 +1,31 @@
 <?php 
-
+include('var/www/html/TechFreaks/config/app.php');
 class User
 {
-    public static function Register($username,$email,$password,$name)
+    private $conn;
+    public function __construct()
     {
-        $query = "";
+        $db = new DatabaseConnection();
+        // die(var_dump($db->));
+        // die(var_dump($db->conn->ping()));
+        $this->conn = $db->conn;
+    }   
+    public function userExist($username)
+    {
+        $query = "SELECT email FROM users WHERE username='$username' LIMIT 1";
+        $result = $this->conn->query($query);
+        return $result;
+    }
+    public function emailExist($email)
+    {
+        $query = "SELECT email FROM users WHERE email='$email' LIMIT 1";
+        $result = $this->conn->query($query);
+        return $result;
+    }
+    public function signUp($name,$username,$email,$password)
+    {
+        $query = "INSERT INTO `TechFreaks`.`users` (`name`, `username`, `email`, `password`, `ver_token`) VALUES ('$name', '$username', '$email', '$password', '$verify_token');";
+        $result = $this->conn->query($query);
+        return $result;
     }
 }
