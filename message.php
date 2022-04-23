3c6d62c60b42d1ce768f2e6fd3e2c7b671e6d453
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-04-23 22:36:36 +0530

    Created Message.php to push alerts

--- message.php
+++ message.php
@@ -1,5 +1,40 @@
-<div class="alert alert-success alert-dismissible fade show border-0" role="alert">
-					<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button> User profile is not complete
-			  </div>
+<?php 
+// session_start();
+
+if(isset($_SESSION['flag']))
+{
+    if($_SESSION['flag'] == 1)
+    {
+            if(isset($_SESSION['message']))
+            { ?>
+            <div class="alert alert-success alert-dismissible fade show border-0" role="alert">
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button><div class="text-center">
+					<?=$_SESSION['message'];?>
+				</div>
+			</div>
+        <?php
+            unset($_SESSION['message']);
+            }
+    }
+    if($_SESSION['flag'] == 2)
+    {
+            if(isset($_SESSION['message']))
+            { ?>
+            <div class="alert alert-danger alert-dismissible fade show border-0" role="alert">
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button><div class="text-center">
+					<?=$_SESSION['message'];?>
+				</div>
+			</div>
+        <?php
+            unset($_SESSION['message']);
+            }
+    }
+}
+
+
+
+?>
