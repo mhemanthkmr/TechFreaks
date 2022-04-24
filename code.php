65cc4215451fca5e2e72419dae95850d0012ef70
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-04-24 22:16:44 +0530

    created profile save bio

--- code.php
+++ code.php
@@ -57,6 +57,43 @@ if(isset($_POST['register_btn'])){
         header("Location: register.php");
     }
 }
+// UPDATE `TechFreaks`.`users` SET `name` = 'ss', `email` = 'harishsekarss1423@gmail.com', `phone` = '454544545' WHERE (`id` = '3');
+
+if(isset($_POST['profile_save_bio']))
+{
+    if(!empty(trim($_POST['email'])) && !empty(trim($_POST['name']))&&!empty(trim($_POST['phone'])) && !empty(trim($_POST['name'])))
+    {
+        // die(print_r($_POST));
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        // die($email);
+        $phone = $_POST['phone'];
+
+        $update_profile = "UPDATE `TechFreaks`.`users` SET `name` = '$name', `email` = '$email', `phone` = '+91$phone' WHERE (`id` = '$id');";
+        // die(print_r($update_profile));
+        $update_profile_run = mysqli_query($con , $update_profile);
+        
+        if($update_profile_run)
+        {
+            $_SESSION['flag'] = 1;
+            $_SESSION['message'] = "Successfully Updated";
+            header("Location: profile.php");
+        }
+        else
+        {
+            $_SESSION['flag'] = 2;
+            $_SESSION['message'] = "Something Wrong";
+            header("Location: profile.php");
+        }
+    }
+    else
+    {
+        $_SESSION['flag'] = 2;
+        $_SESSION['message'] = "All Fields are Mandatory";
+        header("Location: profile.php");
+    }
+}
 if(isset($_POST['logout_btn'])) {
     unset($_SESSION['authenticated']);
     unset($_SESSION['auth_user']);
