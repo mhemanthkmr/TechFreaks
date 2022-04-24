2b1b13575efb5bfc55fa5c2e2bb99879d8fa6e5c
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-04-24 19:50:28 +0530

    Created Code to turn on off lights

--- firebasecode.php
+++ firebasecode.php
@@ -4,7 +4,7 @@ if(isset($_POST['light_off']))
 {
     $light = $_POST['light'];
     $updateData = [
-        'appliance'.$light => "0"
+        'appliance'.$light => "1"
     ];
     $ref_table = "Appliances/";
     $updateData_ref = $database->getReference($ref_table)->update($updateData);
@@ -13,7 +13,33 @@ if(isset($_POST['light_on']))
 {
     $light = $_POST['light'];
     $updateData = [
-        'appliance'.$light => "1"
+        'appliance'.$light => "0"
+    ];
+    $ref_table = "Appliances/";
+    $updateData_ref = $database->getReference($ref_table)->update($updateData);
+}
+if(isset($_POST['light_all_on']))
+{
+    $light = $_POST['light'];
+    $updateData = [
+        'appliance1' => "0",
+        'appliance2' => "0",
+        'appliance3' => "0",
+        'appliance4' => "0",
+        'appliance5' => "0"
+    ];
+    $ref_table = "Appliances/";
+    $updateData_ref = $database->getReference($ref_table)->update($updateData);
+}
+if(isset($_POST['light_all_off']))
+{
+    $light = $_POST['light'];
+    $updateData = [
+        'appliance1' => "1",
+        'appliance2' => "1",
+        'appliance3' => "1",
+        'appliance4' => "1",
+        'appliance5' => "1"
     ];
     $ref_table = "Appliances/";
     $updateData_ref = $database->getReference($ref_table)->update($updateData);
