b0d740ee2f227b0d03198c748740b1e590c0e5e3
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-05-04 06:46:28 +0530

    Added Some Changes and Http is not working

--- Embeded/TechFreaks.ino
+++ Embeded/TechFreaks.ino
@@ -1,4 +1,3 @@
-
 #define DEBUG_SW 1 // Make it 1 to see all debug messages in Serial Monitor
 #include <IRremote.h>
 #include <WiFi.h>
@@ -15,18 +14,31 @@
 
 // IR Receiver Pin
 
-int IR_Recv = 5;
+int IR_Recv = 12;
 
-int status1 = 0, status2 = 0, status3 = 0, status4 = 0, status5 = 0, status6 = 0, status7 = 0;
+int status1 = 0, status2 = 0, status3 = 0, status4 = 0, status5 = 0, status6 = 0, status7 = 0, status8 = 0;
+
+// Start Web Server
+
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
 
 // Decoding Results
 IRrecv irrecv(IR_Recv);
 decode_results results;
 
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0;
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
 // Function Declaration
 void with_internet();
 void without_internet();
-
+void client_server();
 // Pins of Switches
 #define S5 32
 #define S6 35
@@ -39,6 +51,9 @@ void without_internet();
 #define R7 4
 #define R8 22
 #define R9 21
+
+#define FAN 26
+#define ALARM 25
 
 // Define FirebaseESP32 data object
 FirebaseData firebaseData;
@@ -62,6 +77,10 @@ void setup()
     pinMode(R7, OUTPUT);
     pinMode(R8, OUTPUT);
     pinMode(R9, OUTPUT);
+    pinMode(FAN, OUTPUT);
+    pinMode(ALARM, OUTPUT);
+
+    irrecv.enableIRIn(); // Enable IR Receiver
 
     Serial.begin(115200);
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -77,7 +96,125 @@ void loop()
     {
         if (DEBUG_SW)
             Serial.println("Not Connected");
-        without_internet();
+        if (irrecv.decode(&results))
+        {
+            long int decCode = results.value;
+            Serial.println(results.value);
+            switch (results.value)
+            {
+            case 2160019894: // Press 1 to Turn on and Off
+                if (status1 == 0)
+                {
+                    digitalWrite(R5, HIGH);
+                    status1 = 1;
+                }
+                else if (status1 == 1)
+                {
+                    digitalWrite(R5, LOW);
+                    status1 = 0;
+                }
+                break;
+            case 2160052534:
+                if (status2 == 0)
+                {
+                    digitalWrite(R6, HIGH);
+                    status2 = 1;
+                }
+                else if (status2 == 1)
+                {
+                    digitalWrite(R6, LOW);
+                    status2 = 0;
+                }
+                break;
+            case 2160014284: // Press 2 to Turn on and Off
+                if (status3 == 0)
+                {
+                    digitalWrite(R7, HIGH);
+                    status3 = 1;
+                }
+                else if (status3 == 1)
+                {
+                    digitalWrite(R7, LOW);
+                    status3 = 0;
+                }
+                break;
+
+            case 2160030094: // Press 2 to Turn on and Off
+                if (status4 == 0)
+                {
+                    digitalWrite(R8, HIGH);
+                    status4 = 1;
+                }
+                else if (status4 == 1)
+                {
+                    digitalWrite(R8, LOW);
+                    status4 = 0;
+                }
+                break;
+            case 2160062734: // Press 2 to Turn on and Off
+                if (status5 == 0)
+                {
+                    digitalWrite(R9, HIGH);
+                    status5 = 1;
+                }
+                else if (status5 == 1)
+                {
+                    digitalWrite(R9, LOW);
+                    status5 = 0;
+                }
+                break;
+            case 2160006124: // Press 2 to Turn on and Off
+                if (status6 == 0)
+                {
+                    digitalWrite(FAN, HIGH);
+                    status6 = 1;
+                }
+                else if (status6 == 1)
+                {
+                    digitalWrite(FAN, LOW);
+                    status6 = 0;
+                }
+                break;
+            case 2160021934: // Press 2 to Turn on and Off
+                if (status7 == 0)
+                {
+                    digitalWrite(ALARM, HIGH);
+                    status7 = 1;
+                }
+                else if (status7 == 1)
+                {
+                    digitalWrite(ALARM, LOW);
+                    status7 = 0;
+                }
+                break;
+            case 2160016324: // Press 2 to Turn on and Off
+                if (status8 == 0)
+                {
+                    digitalWrite(R5, HIGH);
+                    digitalWrite(R6, HIGH);
+                    digitalWrite(R7, HIGH);
+                    digitalWrite(R8, HIGH);
+                    digitalWrite(R9, HIGH);
+                    digitalWrite(FAN, HIGH);
+                    digitalWrite(ALARM, HIGH);
+                    status8 = 1;
+                }
+                else if (status8 == 1)
+                {
+                    digitalWrite(R5, LOW);
+                    digitalWrite(R6, LOW);
+                    digitalWrite(R7, LOW);
+                    digitalWrite(R8, LOW);
+                    digitalWrite(R9, LOW);
+                    digitalWrite(FAN, LOW);
+                    digitalWrite(ALARM, LOW);
+                    status8 = 0;
+                }
+                break;
+            }
+            irrecv.resume();
+        }
+        delay(10);
     }
 
     else
@@ -85,7 +222,7 @@ void loop()
         if (DEBUG_SW)
             Serial.println(" Connected");
         Data_from_firebase();
-        with_internet();
+        // with_internet();
     }
 }
 
@@ -166,6 +303,40 @@ void Data_from_firebase()
             Serial.println(firebaseData.stringData());
 
         if (firebaseData.stringData() == "1")
+        {
+            digitalWrite(R9, HIGH);
+        }
+        else
+        {
+            digitalWrite(R9, LOW);
+        }
+    }
+
+    if (Firebase.getString(firebaseData, "/Appliances/appliance6"))
+    {
+        if (DEBUG_SW)
+            Serial.print("Relay6 - ");
+        if (DEBUG_SW)
+            Serial.println(firebaseData.stringData());
+
+        if (firebaseData.stringData() == "0")
+        {
+            digitalWrite(R9, HIGH);
+        }
+        else
+        {
+            digitalWrite(R9, LOW);
+        }
+    }
+
+    if (Firebase.getString(firebaseData, "/Appliances/appliance7"))
+    {
+        if (DEBUG_SW)
+            Serial.print("Relay7 - ");
+        if (DEBUG_SW)
+            Serial.println(firebaseData.stringData());
+
+        if (firebaseData.stringData() == "0")
         {
             digitalWrite(R9, HIGH);
         }
@@ -336,14 +507,13 @@ void with_internet()
 
 void without_internet()
 {
-    // FOR SWITCH
     if (irrecv.decode(&results))
     {
         long int decCode = results.value;
         Serial.println(results.value);
         switch (results.value)
         {
-        case 216001238254: // Press 1 to Turn on and Off
+        case 2160019894: // Press 1 to Turn on and Off
             if (status1 == 0)
             {
                 digitalWrite(R5, HIGH);
@@ -355,7 +525,7 @@ void without_internet()
                 status1 = 0;
             }
             break;
-        case 2160323009694:
+        case 2160052534:
             if (status2 == 0)
             {
                 digitalWrite(R6, HIGH);
@@ -367,7 +537,7 @@ void without_internet()
                 status2 = 0;
             }
             break;
-        case 216002328054: // Press 2 to Turn on and Off
+        case 2160014284: // Press 2 to Turn on and Off
             if (status3 == 0)
             {
                 digitalWrite(R7, HIGH);
@@ -380,7 +550,7 @@ void without_internet()
             }
             break;
 
-        case 2160023234054: // Press 2 to Turn on and Off
+        case 2160030094: // Press 2 to Turn on and Off
             if (status4 == 0)
             {
                 digitalWrite(R8, HIGH);
@@ -392,7 +562,7 @@ void without_internet()
                 status4 = 0;
             }
             break;
-        case 2160342028054: // Press 2 to Turn on and Off
+        case 2160062734: // Press 2 to Turn on and Off
             if (status5 == 0)
             {
                 digitalWrite(R9, HIGH);
@@ -404,30 +574,113 @@ void without_internet()
                 status5 = 0;
             }
             break;
-        case 21603420254: // Press 2 to Turn on and Off
+        case 2160006124: // Press 2 to Turn on and Off
             if (status6 == 0)
             {
-                digitalWrite(R9, HIGH);
+                digitalWrite(FAN, HIGH);
                 status6 = 1;
             }
             else if (status6 == 1)
             {
-                digitalWrite(R9, LOW);
+                digitalWrite(FAN, LOW);
                 status6 = 0;
             }
             break;
-        case 2160432028054: // Press 2 to Turn on and Off
+        case 2160021934: // Press 2 to Turn on and Off
             if (status7 == 0)
             {
-                digitalWrite(R9, HIGH);
+                digitalWrite(ALARM, HIGH);
                 status7 = 1;
             }
             else if (status7 == 1)
             {
-                digitalWrite(R9, LOW);
+                digitalWrite(ALARM, LOW);
                 status7 = 0;
             }
             break;
         }
+    }
+}
+
+void client_server()
+{
+    WiFiClient client = server.available(); // Listen for incoming clients
+    if (client)
+    { // If a new client connects,
+        currentTime = millis();
+        previousTime = currentTime;
+        Serial.println("New Client."); // print a message out in the serial port
+        String currentLine = "";       // make a String to hold incoming data from the client
+        while (client.connected() && currentTime - previousTime <= timeoutTime)
+        { // loop while the client's connected
+            currentTime = millis();
+            if (client.available())
+            {                           // if there's bytes to read from the client,
+                char c = client.read(); // read a byte, then
+                Serial.write(c);        // print it out the serial monitor
+                header += c;
+                if (c == '\n')
+                { // if the byte is a newline character
+                    // if the current line is blank, you got two newline characters in a row.
+                    // that's the end of the client HTTP request, so send a response:
+                    if (currentLine.length() == 0)
+                    {
+                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                        // and a content-type so the client knows what's coming, then a blank line:
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println("Connection: close");
+                        client.println();
+
+                        // turns the GPIOs on and off
+                        if (header.indexOf("GET /1/on") >= 0)
+                        {
+                            Serial.println("GPIO 1 on");
+                            // output26State = "on";
+                            digitalWrite(R5, HIGH);
+                            String Value1 = "1";
+                            json.set("/appliance1", Value1);
+                            Firebase.updateNode(firebaseData, "/Appliances", json);
+                        }
+                        else if (header.indexOf("GET /1/off") >= 0)
+                        {
+                            Serial.println("GPIO 1 off");
+                            // output26State = "off";
+                            digitalWrite(R5, LOW);
+                        }
+                        else if (header.indexOf("GET /2/on") >= 0)
+                        {
+                            Serial.println("GPIO 2 on");
+                            // output27State = "on";
+                            digitalWrite(R6, HIGH);
+                        }
+                        else if (header.indexOf("GET /2/off") >= 0)
+                        {
+                            Serial.println("GPIO R6 off");
+                            // output27State = "off";
+                            digitalWrite(R6, LOW);
+                        }
+
+                        // Display the HTML web page
+                        // Break out of the while loop
+                        break;
+                    }
+                    else
+                    { // if you got a newline, then clear currentLine
+                        currentLine = "";
+                    }
+                }
+                else if (c != '\r')
+                {                     // if you got anything else but a carriage return character,
+                    currentLine += c; // add it to the end of the currentLine
+                }
+            }
+        }
+        // Clear the header variable
+        header = "";
+        // Close the connection
+        client.stop();
+        Serial.println("Client disconnected.");
+        Serial.println("");
     }
 }
