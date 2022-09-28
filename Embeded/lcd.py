82616234dfa99ef30ba20da2d91fa5873dc4c5d3
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-09-28 18:40:33 +0530

    Added a lcd.py and get info from api

--- Embeded/lcd.py
+++ Embeded/lcd.py
@@ -1,7 +1,27 @@
+import json
+import requests
 from signal import signal, SIGTERM, SIGHUP, pause
 from time import sleep
 from rpi_lcd import LCD
 lcd = LCD()
+
+
+def api():
+    reqUrl = "https://iot-project-2f20e-default-rtdb.firebaseio.com/Appliances.json?auth=DCJqdn05xseUfXIve8SzxWu0wMEpkdtn1gE2WzRG"
+
+    headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+    }
+
+    payload = ""
+
+    response = requests.request(
+        "GET", reqUrl, data=payload,  headers=headersList)
+    aList = response.json()
+    return aList
+
+# print(aList['temperature'])
 
 
 def safe_exit(signum, frame):
@@ -12,17 +32,18 @@ while True:
     try:
         signal(SIGTERM, safe_exit)
         signal(SIGHUP, safe_exit)
+        aList = api()
         lcd.text("Ethic Electronics", 1)
         lcd.text("Air Purity : 76%", 2)
         sleep(5)
-        lcd.text("Temperature:36°", 1)
-        lcd.text("Humidity : 83%", 2)
+        lcd.text("Temperature:"+str(aList['temperature'])+"C", 1)
+        lcd.text("Humidity :"+str(aList['humidity']), 2)
         sleep(5)
         lcd.text("Ethic Electronics", 1)
         lcd.text("Air Purity : 76%", 2)
         sleep(5)
-        lcd.text("Temperature:36°", 1)
-        lcd.text("Humidity : 83%", 2)
+        lcd.text("Temperature:"+str(aList['temperature'])+"C", 1)
+        lcd.text("Humidity :"+str(aList['humidity']), 2)
         sleep(5)
     except KeyboardInterrupt:
         pass
