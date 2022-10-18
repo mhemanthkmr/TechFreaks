92c07bd28a6ad30681c31aa5043dd405e1e115ea
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-10-18 15:31:18 +0530

    Added a MQTT for Home Page and Lights

---
+++ homemqtt.js
@@ -0,0 +1,259 @@
+var url = "https://io.adafruit.com/api/v2/mhemanthkmr143/feeds/r1/data";
+$(document).ready(function () {
+  var button;
+  var box;
+  $("#switch-light-1").click(function () {
+    if (!$(this).is(":checked")) {
+      button = 1;
+      box = 1;
+      console.log(button);
+      console.log(box);
+      var url = "https://io.adafruit.com/api/v2/mhemanthkmr143/feeds/r1/data";
+      $.ajax({
+        url: url,
+        type: "post",
+        data: { value: 0 },
+        headers: { "X-AIO-Key": "aio_RXxM62BYFcLuQorYtU44MIsx1ZgX" },
+        // contentType: 'application/json',
+        dataType: "json",
+        success: function (data) {
+          console.log(data);
+        },
+      });
+    } else {
+      var url = "https://io.adafruit.com/api/v2/mhemanthkmr143/feeds/r1/data";
+      $.ajax({
+        url: url,
+        type: "post",
+        data: { value: 1 },
+        headers: { "X-AIO-Key": "aio_RXxM62BYFcLuQorYtU44MIsx1ZgX" },
+        // contentType: 'application/json',
+        dataType: "json",
+        success: function (data) {
+          console.log(data);
+        },
+      });
+    }
+  });
+
+  $("#switch-light-2").click(function () {
+    if (!$(this).is(":checked")) {
+      button = 2;
+      console.log(button);
+      box = 1;
+      console.log(box);
+      var url =
+        "https://io.adafruit.com/api/v2/mhemanthkmr143/feeds/relay2/data";
+      $.ajax({
+        url: url,
+        type: "post",
+        data: { value: 0 },
+        headers: { "X-AIO-Key": "aio_RXxM62BYFcLuQorYtU44MIsx1ZgX" },
+        // contentType: 'application/json',
+        dataType: "json",
+        success: function (data) {
+          console.log(data);
+        },
+      });
+    } else {
+      var url =
+        "https://io.adafruit.com/api/v2/mhemanthkmr143/feeds/relay2/data";
+      $.ajax({
+        url: url,
+        type: "post",
+        data: { value: 1 },
+        headers: { "X-AIO-Key": "aio_RXxM62BYFcLuQorYtU44MIsx1ZgX" },
+        // contentType: 'application/json',
+        dataType: "json",
+        success: function (data) {
+          console.log(data);
+        },
+      });
+    }
+  });
+
+  $("#switch-light-3").click(function () {
+    if (!$(this).is(":checked")) {
+      button = 3;
+      console.log(button);
+      box = 1;
+      console.log(box);
+      var url =
+        "https://io.adafruit.com/api/v2/mhemanthkmr143/feeds/relay3/data";
+      $.ajax({
+        url: url,
+        type: "post",
+        data: { value: 0 },
+        headers: { "X-AIO-Key": "aio_RXxM62BYFcLuQorYtU44MIsx1ZgX" },
+        // contentType: 'application/json',
+        dataType: "json",
+        success: function (data) {
+          console.log(data);
+        },
+      });
+    } else {
+      var url =
+        "https://io.adafruit.com/api/v2/mhemanthkmr143/feeds/relay3/data";
+      $.ajax({
+        url: url,
+        type: "post",
+        data: { value: 1 },
+        headers: { "X-AIO-Key": "aio_RXxM62BYFcLuQorYtU44MIsx1ZgX" },
+        // contentType: 'application/json',
+        dataType: "json",
+        success: function (data) {
+          console.log(data);
+        },
+      });
+    }
+  });
+
+  $("#switch-light-4").click(function () {
+    if (!$(this).is(":checked")) {
+      button = 4;
+      console.log(button);
+      box = 1;
+      console.log(box);
+      var url =
+        "https://io.adafruit.com/api/v2/mhemanthkmr143/feeds/relay4/data";
+      $.ajax({
+        url: url,
+        type: "post",
+        data: { value: 0 },
+        headers: { "X-AIO-Key": "aio_RXxM62BYFcLuQorYtU44MIsx1ZgX" },
+        // contentType: 'application/json',
+        dataType: "json",
+        success: function (data) {
+          console.log(data);
+        },
+      });
+    } else {
+      var url =
+        "https://io.adafruit.com/api/v2/mhemanthkmr143/feeds/relay4/data";
+      $.ajax({
+        url: url,
+        type: "post",
+        data: { value: 1 },
+        headers: { "X-AIO-Key": "aio_RXxM62BYFcLuQorYtU44MIsx1ZgX" },
+        // contentType: 'application/json',
+        dataType: "json",
+        success: function (data) {
+          console.log(data);
+        },
+      });
+    }
+  });
+
+  $("#switch-light-5").click(function () {
+    if (!$(this).is(":checked")) {
+      button = 4;
+      console.log(button);
+      box = 1;
+      console.log(box);
+      var url =
+        "https://io.adafruit.com/api/v2/mhemanthkmr143/feeds/relay5/data";
+      $.ajax({
+        url: url,
+        type: "post",
+        data: { value: 0 },
+        headers: { "X-AIO-Key": "aio_RXxM62BYFcLuQorYtU44MIsx1ZgX" },
+        // contentType: 'application/json',
+        dataType: "json",
+        success: function (data) {
+          console.log(data);
+        },
+      });
+    } else {
+      var url =
+        "https://io.adafruit.com/api/v2/mhemanthkmr143/feeds/relay5/data";
+      $.ajax({
+        url: url,
+        type: "post",
+        data: { value: 1 },
+        headers: { "X-AIO-Key": "aio_RXxM62BYFcLuQorYtU44MIsx1ZgX" },
+        // contentType: 'application/json',
+        dataType: "json",
+        success: function (data) {
+          console.log(data);
+        },
+      });
+    }
+  });
+
+  $(".click").click(function () {
+    if (box == 1) {
+      $.ajax({
+        type: "POST",
+        url: "firebasecode.php",
+        data: {
+          light_off: true,
+        },
+      });
+    }
+  });
+
+  $("#checkbox1").click(function () {
+    if ($(this).is(":checked")) {
+      button = 1;
+      box = 2;
+      console.log(button);
+      console.log(box);
+      $(".show1").attr("src", "led1.jpg");
+      $("#show1").addClass("text-success").removeClass("text-danger");
+      $("#text1 span").text("LIGHT 1 ON");
+    }
+  });
+
+  $("#checkbox2").click(function () {
+    if ($(this).is(":checked")) {
+      button = 2;
+      console.log(button);
+      box = 2;
+      console.log(box);
+      $(".show2").attr("src", "led1.jpg");
+      $("#show2").addClass("text-success").removeClass("text-danger");
+      $("#text2 span").text("LIGHT 2 ON");
+    }
+  });
+
+  $("#checkbox3").click(function () {
+    if ($(this).is(":checked")) {
+      button = 3;
+      console.log(button);
+      box = 2;
+      console.log(box);
+      $(".show3").attr("src", "led1.jpg");
+      $("#show3").addClass("text-success").removeClass("text-danger");
+      $("#text3 span").text("LIGHT 3 ON");
+    }
+  });
+
+  $("#checkbox4").click(function () {
+    if ($(this).is(":checked")) {
+      button = 4;
+      console.log(button);
+      box = 2;
+      console.log(box);
+      $(".show4").attr("src", "led1.jpg");
+      $("#show4").addClass("text-success").removeClass("text-danger");
+      $("#text4 span").text("LIGHT 4 ON");
+    }
+  });
+
+  $(".click").click(function () {
+    if (box == 2) {
+      var url = "https://io.adafruit.com/api/v2/mhemanthkmr143/feeds/r1/data";
+      $.ajax({
+        url: url,
+        type: "post",
+        data: { value: 0 },
+        headers: { "X-AIO-Key": "aio_RXxM62BYFcLuQorYtU44MIsx1ZgX" },
+        // contentType: 'application/json',
+        dataType: "json",
+        success: function (data) {
+          console.log(data);
+        },
+      });
+    }
+  });
+});
